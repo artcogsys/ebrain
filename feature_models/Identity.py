9081a50eccc66658b50fda4ad1409f680b700e3a
edward339 <edward339@gmail.com>
2016-02-01 10:46:00 +0100

    Inheritance for FM and RM

--- feature_models/Identity.py
+++ feature_models/Identity.py
@@ -1,16 +1,12 @@
 #Identity feature model
-<<<<<<< Updated upstream
 
 # NOTE: The feature models will likely be prepared outside the toolbox. The toolbox
 # will provide a way to access them, e.g. by loading preexisting features or by 
 # passing the input stimuli through a pretrained model. 
 
-class Identity(object):
-=======
 from feature_models.FeatureModel import FeatureModel
 
 class Identity(FeatureModel):
->>>>>>> Stashed changes
         
     def fit(self,X): #Fit model
         pass
