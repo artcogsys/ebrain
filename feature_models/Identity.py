6dc3731f6cf6297611ab8a442e27fa5bf2be8665
Katja <mue.katja@gmail.com>
2016-02-01 02:27:54 +0100

    Minor: Added a comment.

--- feature_models/Identity.py
+++ feature_models/Identity.py
@@ -1,4 +1,9 @@
 #Identity feature model
+
+# NOTE: The feature models will likely be prepared outside the toolbox. The toolbox
+# will provide a way to access them, e.g. by loading preexisting features or by 
+# passing the input stimuli through a pretrained model. 
+
 class Identity(object):
         
     def fit(self,X): #Fit model
